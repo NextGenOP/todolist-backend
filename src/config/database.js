480ec43cbff9d2d0a2d3d79c647ecca2a6659f73
NextGenOP <60586402+NextGenOP@users.noreply.github.com>
2023-08-14 11:48:09 +0700

    database config: forgot to import fs :)

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const fs = require('fs');
 const { DB_HOST_DATABASE, DB_NAME, DB_USER, DB_PASSWORD,DB_HOST_DATABASE_PRODUCTION,DB_USER_PRODUCTION,DB_PASSWORD_PRODUCTION,DB_NAME_PRODUCTION,DB_SSL_IS,DB_SSL_PRODUCTION_IS } = process.env;
 const rootCert = fs.readFileSync('/etc/ssl/certs/ca-certificates.crt');
 
